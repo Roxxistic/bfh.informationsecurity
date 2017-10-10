49b5d90d46b01a0710e142e5c04ed2e8687d6129
Marc Rey <roxxistic@gmail.com>
2017-10-10 06:52:42 +0200

    operating with flat char array.

--- src/CipherBlockChaining.java
+++ src/CipherBlockChaining.java
@@ -4,19 +4,20 @@
  */
 public class CipherBlockChaining {
 
+    private final int BLOCK_LENGTH = 3;
+
     public static void main(String[] args) {
         CipherBlockChaining cbc = new CipherBlockChaining();
-
         char[] text = "abcdefghijk".toCharArray();
-        char[] iv = "xyz".toCharArray();
-        int cesarKey = 5;
-        char[] encrypted = cbc.encrypt(text, iv, cesarKey);
+        char cesarKey = ' ';
+        char[] encrypted = cbc.encrypt(text, cesarKey);
         cbc.printChars(encrypted);
-        char[] decrypted = cbc.decrypt(encrypted, iv, cesarKey);
+        char[] decrypted = cbc.decrypt(encrypted, cesarKey);
         cbc.printChars(decrypted);
     }
 
-    private char[] encrypt(char[] plaintext, char[] iv, int cesarKey)  {
+    private char[] encrypt(char[] plaintext, char cesarKey)  {
+        char[] iv = this.getInitializationVector();
         char[] encrypted = plaintext.length % iv.length == 0 ? new char[plaintext.length + iv.length] : new char[((plaintext.length / iv.length)*iv.length+iv.length) + iv.length];
         System.arraycopy(iv, 0, encrypted, 0, iv.length);
         System.arraycopy(plaintext, 0, encrypted, iv.length, plaintext.length);
@@ -25,52 +26,23 @@ public class CipherBlockChaining {
                 encrypted[i] = (char) (encrypted[i - iv.length] ^ encrypted[i]);
             }
             // apply key
-            //encrypted[i] = (char)(encrypted[i] + (char)cesarKey);
         }
         return encrypted;
     }
 
-    private void printXor(char[] a, char[] b, char[] xor){
-        printBin(a);
-        System.out.print(" XOR ");
-        printBin(b);
-        System.out.print(" IS ");
-        printBin(xor);
-        System.out.println();
-    }
+    private char[] decrypt(char[] cipherText, char cesarKey){
+        char[] iv = this.getInitializationVector();
+        char[] decrypted = new char[cipherText.length];
+        for(int i = 0; i < decrypted.length; i++){
+            // apply key
+            if(i>=iv.length){
+                decrypted[i] = (char) (cipherText[i - iv.length] ^ cipherText[i]);
+            } else {
+                decrypted[i] = cipherText[i];
+            }
 
-    private void printInt(char[][] a){
-        int blockCount = a.length;
-
-        // print
-        for(int i = 0; i < blockCount; i++){
-            printInt(a[i]);
-            System.out.print("   ");
         }
-        System.out.println();
-    }
-
-
-    private void printChars(char[][] a){
-        int blockCount = a.length;
-
-        // print
-        for(int i = 0; i < blockCount; i++){
-            printChars(a[i]);
-            System.out.print("   ");
-        }
-        System.out.println();
-    }
-
-    private void printBin(char[][] a){
-        int blockCount = a.length;
-
-        // print
-        for(int i = 0; i < blockCount; i++){
-            printBin(a[i]);
-            System.out.print("   ");
-        }
-        System.out.println();
+        return decrypted;
     }
 
     private void printChars(char[] a){
@@ -92,27 +64,8 @@ public class CipherBlockChaining {
         }
     }
 
-    private void printInt(char[] a){
-        int blockSize = a.length;
-        for(int j = 0; j < blockSize; j++){
-            System.out.print((int)a[j] + " ");
-        }
+    private char[] getInitializationVector(){
+        return "xyz".toCharArray();
     }
-
-    private char[] decrypt(char[] cipherText, char[] iv, int cesarKey){
-        char[] decrypted = new char[cipherText.length];
-        for(int i = 0; i < decrypted.length; i++){
-            if(i>=iv.length){
-                decrypted[i] = (char) (cipherText[i - iv.length] ^ cipherText[i]);
-            } else {
-                decrypted[i] = cipherText[i];
-            }
-            // apply key
-            //decrypted[i] = (char)(decrypted[i] - (char)cesarKey);
-        }
-        return decrypted;
-    }
-
-
 
 }
